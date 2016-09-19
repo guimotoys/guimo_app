56987aa821d043d17742973974a8f06a157d13c3
Jorge Junior <jorgedjr21@gmail.com>
2016-09-19 18:28:02 -0300

    Finalized Conversa Screen. Need to integrate with hardware

--- www/js/Controllers/ConversaController.js
+++ www/js/Controllers/ConversaController.js
@@ -1,12 +1,27 @@
-app.controller('ConversaController',function($scope,$rootScope,$ionicPlatform,$ionicHistory){
-
+app.controller('ConversaController',function($scope,$rootScope,$ionicPlatform,$ionicHistory,$timeout){
+  $scope.respostas = ['Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze'];
   $ionicPlatform.ready(function(){
-    $scope.voltar = function(){
-      console.log("voltar");
-      $ionicHistory.goBack();
-      console.log($ionicHistory.backView());
+    $scope.resposta = '';
+    $scope.respondendo = false;
+    if(window.cordova){
 
     }
 
+    $scope.conversar = function(min,max){
+      $scope.respondendo = true;
+      $timeout(function(){
+        $scope.respondendo = false;
+        var rand = Math.floor(Math.random() * (max - min + 1)) + min;
+        $scope.resposta = $scope.respostas[rand];
+        console.log(rand);
+      },3000);
+    }
+
+
   });
 });
+
+
+//0 1 2 3 4
+//5 6 7 8 9
+//10 11 12 13 14
