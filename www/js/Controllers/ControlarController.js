0bb73090ee5a5c8a62e78c67c9d157627cc76be9
Jorge <jorgedjr21@gmail.com>
2016-11-13 18:58:02 -0200

    worked fixing virtualjoystick errors in ionic

--- www/js/Controllers/ControlarController.js
+++ www/js/Controllers/ControlarController.js
@@ -1,11 +1,39 @@
-app.controller('ControlarController',function($scope,$rootScope,$ionicPlatform,$ionicHistory,$timeout){
+app.controller('ControlarController',function($scope,$rootScope,$ionicPlatform,$ionicHistory,$interval){
 
 
     $ionicPlatform.ready(function(){
 
       if(window.cordova){
-
+        screen.lockOrientation('landscape');
       }
+
+      console.log("touchscreen is", VirtualJoystick.touchScreenAvailable() ? "available" : "not available");
+
+			var joystick	= new VirtualJoystick({
+				container	: document.getElementById('container'),
+				mouseSupport	: true,
+        stationaryBase: true,
+        baseX: 140,
+        baseY: 130,
+        limitStickTravel: true,
+        stickRadius: 100
+			});
+
+			joystick.addEventListener('touchStart', function(){
+				console.log('down')
+			})
+			joystick.addEventListener('touchEnd', function(){
+				console.log('up')
+			})
+
+			$interval(function(){
+
+				console.log('Result:'
+					+ (joystick.right()	? ' right'	: '')
+					+ (joystick.up()	? ' up'		: '')
+					+ (joystick.left()	? ' left'	: '')
+					+ (joystick.down()	? ' down' 	: ''));
+			}, 700);
 
     });
 });
